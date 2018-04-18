14db644214ae92ff6e59e8a17366e27686c9155f
ygperez <35947248+ygperez@users.noreply.github.com>
2018-04-17 22:18:57 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,17 @@
+import math
+import sys
+
+def add(x,y):
+    print "Add = ", x+y
+
+def diff(x,y):
+    print "Subtract = ", x-y
+
+def product(x,y):
+    print "Product = ", x*y
+
+def greatest(x,y):
+    if x>y: 
+        print "x is greater than y"
+    if y>x:
+        print "y is greater than x"
